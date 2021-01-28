c83fd6675fab863a57dcdbb9ec247c3e9be21cef
QingQiz <sofeeys@outlook.com>
2021-01-28 21:09:38 +0800

    update Exp/MosaicPuzzle.py

--- Exp/MosaicPuzzle.py
+++ Exp/MosaicPuzzle.py
@@ -3,103 +3,160 @@
 
 """
 用图库中的图拼成目标图片
-
-图片特征值用的图片平均值
 """
 
 
-import numpy, os
+import os
+import PIL
+import rich
+import argh
+import numpy
 from PIL import Image
+from rich.console import Console
+from rich.progress import Progress
 
 
-# 目标图片路径
-target      = './Pictures/WallP/P001.png'
-result_path = './res.jpg'
+class ImageLoader:
+    def __init__(self, logger, **kwargs):
+        self._cache = {}
+        self._feature = {}
+        self._kwargs = kwargs
+        self._logger = logger
 
-# 图库路径
-img_path = './Pictures/pic/pho'
-img_cnt  = 200
-# 拼图时图库中图片的大小
-ps = 40
+    def loadImg(self, path, crop=True, zoom=False):
+        if (res := self._cache.get(path)) is not None:
+            return res
 
-# 目标图片缩放级别
-scale = 10
+        try:
+            # self._logger(f'Loading {path}')
 
-cache = {}
+            img = Image.open(path)
+            w, h = img.size
 
+            if zoom:
+                w //= self._kwargs['zoom']
+                h //= self._kwargs['zoom']
+                img = img.resize((w, h))
+            if crop:
+                size = min(w, h, self._kwargs['size'])
+                img = img.crop((0, 0, size, size))
+                img = img.resize((size, size))
 
-def load_image(pic_path):
-    if cache.get(pic_path) is not None:
-        return cache[pic_path]
+            self._cache[path] = img
+            return img
+        except OSError as e:
+            self._logger('[red][b]' + str(e).strip())
+            exit(-1)
 
-    x = Image.open(pic_path)
-    w, h = x.size
-    x = x.crop((0, 0, min(w, h), min(w, h)))
-    x = x.resize((ps, ps))
+    def imgFeature(self, path):
+        if (res := self._feature.get(path)) is not None:
+            return res
 
-    cache[pic_path] = x
-    return x
+        # self._logger(f'Calculating the feature of image {path}')
+        img = self.loadImg(path)
+        img = numpy.array(img)
+        feature = img.mean(axis=1).mean(axis=0)
+        self._feature[path] = feature
+        return feature
 
+    def cloestImage(self, pixel):
+        def MSE(p1, p2):
+            return ((p1 - p2) ** 2).sum()
 
-def image_mean(pic_path: str):
-    img = numpy.array(load_image(pic_path))
-    res = img.mean(axis=1).mean(axis=0)
-    return res
+        # self._logger(f'Finding cloest image for pixel {pixel}')
 
+        res = None
+        cmp = 2560000
 
-def find_cloest_image(pixel, l):
-    import random
-
-    def MSE(p0, p1):
-        return ((p0 - p1) ** 2).sum()
-
-    res = None
-    cmp = 2560000
-
-    for i, j in l:
-        loss = MSE(j, pixel)
-
-        if loss < cmp:
-            if random.randint(0, 10) <= 2 and res is not None:
+        for img in self._cache:
+            if img == self._kwargs['target']:
                 continue
-            res = i
-            cmp = loss
-    return res
+
+            feature = self.imgFeature(img)
+
+            loss = MSE(feature, pixel)
+
+            if loss < cmp :
+                res = img
+                cmp = loss
+        return res
 
 
-pics = list(map(lambda x: img_path + '/' + x, os.listdir(img_path)[:img_cnt]))
-pic_mean = list(map(image_mean, pics))
-pic_mean = tuple(zip(pics, pic_mean))
+def run(**kwargs):
+    target      = kwargs['target']
+    output      = kwargs['output']
+    dataset     = kwargs['dataset']
+    dataset_cnt = kwargs['img_cnt']
+    size        = kwargs['size']
+
+    with Progress() as prog:
+        logger = prog.console.print
+        dataLoader = ImageLoader(logger, **kwargs)
+
+        imgs = os.listdir(dataset)[:dataset_cnt]
+
+        task_load_image = prog.add_task(f'Loading images [000/{dataset_cnt}]', total=dataset_cnt)
+
+        cnt = 1
+        for i in imgs:
+            dataLoader.loadImg(f'{dataset}/{i}')
+            prog.update(task_load_image, advance=1, description=f'Loading images [{cnt:0>3}/{dataset_cnt}]')
+            cnt += 1
+
+        task_calc_feature = prog.add_task(f'Calculating image feature [000/{dataset_cnt}]', total=dataset_cnt)
+
+        cnt = 1
+        for i in imgs:
+            dataLoader.imgFeature(f'{dataset}/{i}')
+            prog.update(task_calc_feature, advance=1, description=f'Calculating image feature [{cnt:0>3}/{dataset_cnt}]')
+            cnt += 1
+
+        logger(f'[green]Loading target image...')
+        target_img = dataLoader.loadImg(target, crop=False, zoom=True)
+        target_img = numpy.array(target_img)
+
+        shape = target_img.shape
+        logger(f'[green]The shape of target image is [/green]{shape}')
+
+        task_find_cloest = prog.add_task(f'Finding cloest image [00000/{shape[0] * shape[1]}]', total=shape[0] * shape[1])
+
+        cloest_image = []
+        cnt = 1
+        for i in range(shape[0]):
+            cloest_image.append([])
+            for j in range(shape[1]):
+                img = dataLoader.cloestImage(target_img[i][j])
+                cloest_image[-1].append(img)
+                prog.update(task_find_cloest, advance=1, description=f'Finding cloest image [{cnt:0>6}/{shape[0] * shape[1]:0>6}]')
+                cnt += 1
+
+        task_paste_image = prog.add_task(f'Pasting image [00000/{shape[0] * shape[1]}]', total=shape[0] * shape[1])
+
+        logger(f'[green]Creating new image, size: {(shape[0] * size, shape[1] * size)}')
+
+        img_res = Image.new('RGB', (shape[1] * size, shape[0] * size))
+
+        cnt = 1
+        for i in range(shape[0]):
+            for j in range(shape[1]):
+                # logger(f'Pasting image {cloest_image[i][j]} to {(i * size, j * size)}')
+                img_res.paste(dataLoader.loadImg(cloest_image[i][j]), (j * size, i * size))
+                prog.update(task_paste_image, advance=1, description=f'Pasting image [{cnt:0>6}/{shape[0] * shape[1]:0>6}]')
+                cnt += 1
+
+        logger(f'[green]Saving image to {output}')
+        img_res.save(output)
 
 
-target = Image.open(target)
-tw, th = target.size
-tw //= scale
-th //= scale
-target = target.resize((tw, th))
-target = numpy.array(target)
+if __name__ == "__main__":
+    parser = argh.ArghParser()
+    parser.add_argument('-t', '--target', required=True, help='target image')
+    parser.add_argument('-o', '--output', required=True, help='output')
+    parser.add_argument('-d', '--dataset', required=True, help='dataset path')
+    parser.add_argument('-n', '--img-cnt', required=False, default=100, type=int, help='the count of images to use in dataset')
+    parser.add_argument('-s', '--size', required=False, default=30, type=int, help='the size of images in dataset')
+    parser.add_argument('-z', '--zoom', required=False, default=10, type=int, help='zoom of target image')
 
-s_i, s_j, _ = target.shape
-res = []
+    parser.set_default_command(run)
 
-
-for i in range(s_i):
-    res.append([])
-    for j in range(s_j):
-        pic = find_cloest_image(target[i][j], pic_mean)
-        res[-1].append(pic)
-
-
-new_im = Image.new('RGB', (tw * ps, th * ps))
-
-idx_r = 0
-for i in range(len(res)):
-    idx_c = 0
-    for img in res[i]:
-        new_im.paste(load_image(img), (idx_c, idx_r))
-        idx_c += ps
-    idx_r += ps
-
-
-new_im.save(result_path)
-
+    parser.dispatch()
